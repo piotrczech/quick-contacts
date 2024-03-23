91e56be92f152899857b3736d83629c7b421fa4f
Piotr Czech <czechpiotr2701@gmail.com>
2024-03-23 20:42:09 +0100

    feat: create controller and mock for POST one contact info

--- src/sw/mocks/contact-info/controller.js
+++ src/sw/mocks/contact-info/controller.js
@@ -1,14 +1,18 @@
+import ContactInfo from '@/models/contactInfoModel'
+
 /**
  * ContactInfoController class responsible for managing contact information.
  */
 class ContactInfoController {
   /**
    * Constructs a new ContactInfoController instance.
+   *
    * @param {Array} defaultValues Array of default contact info values.
    */
   constructor(defaultValues = []) {
     /**
      * Map containing all existing contact info.
+     *
      * @type {Map}
      */
     this.contactInfo = new Map()
@@ -20,10 +24,30 @@ class ContactInfoController {
 
   /**
    * Retrieves all contact info.
+   *
    * @returns {Array} Array of contact info objects.
    */
   getAll() {
     return Array.from(this.contactInfo.values())
+  }
+
+  /**
+   * Adds a new contact.
+   *
+   * @param {Object} data Contact data.
+   * @returns {string|null} ID of the newly added contact, or null if failed.
+   */
+  addOne(requestData) {
+    if (!requestData.firstName || !requestData.phoneNumber) {
+      throw new Error('First name and phone number are required.')
+    }
+
+    const newId = Math.max(...this.contactInfo.keys(), 0) + 1
+    const newContactInfo = new ContactInfo({ ...requestData, id: newId })
+
+    this.contactInfo.set(newId, newContactInfo)
+
+    return newContactInfo
   }
 }
 
--- src/sw/mocks/contact-info/index.js
+++ src/sw/mocks/contact-info/index.js
@@ -7,6 +7,7 @@ const contactInfoController = new ContactInfoController(contactInfoDefaultValues
 
 /**
  * Handlers for managing contact info endpoints.
+ *
  * @type {Array}
  */
 const handlers = [
@@ -15,6 +16,21 @@ const handlers = [
    */
   http.get(`${API_PREFIX}/contact-info`, () => {
     return HttpResponse.json(contactInfoController.getAll())
+  }),
+
+  /**
+   * Handler for adding new contact info.
+   *
+   * @param {Object} request HTTP request containing new contact info data.
+   */
+  http.post(`${API_PREFIX}/contact-info`, async ({ request }) => {
+    try {
+      const newContactInfoData = await request.json()
+      const newContactInfo = contactInfoController.addOne(newContactInfoData)
+      return HttpResponse.json(newContactInfo, { status: 201 })
+    } catch (error) {
+      return HttpResponse.error(error.message, { status: 409 })
+    }
   })
 ]
 
