c3962c4c51751ad5fb8fab3d41ff8d5675f85a26
Piotr Czech <czechpiotr2701@gmail.com>
2024-03-23 19:00:04 +0100

    feat: Add PWA package and manifest declaration

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,43 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VueDevTools from 'vite-plugin-vue-devtools'
 import vuetify from 'vite-plugin-vuetify'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vuetify({ autoImport: true }),
-    VueDevTools(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'QuickContacts',
+        short_name: 'QuickContacts',
+        description: ' A phonebook app that enables fast contact search, addition, and editing.',
+        theme_color: '#ffffff',
+        background_color: '#212121',
+        icons: [
+          {
+            purpose: 'maskable',
+            sizes: '512x512',
+            src: '/icons/icon512_maskable.png',
+            type: 'image/png'
+          },
+          {
+            purpose: 'any',
+            sizes: '512x512',
+            src: '/icons/icon512_rounded.png',
+            type: 'image/png'
+          }
+        ],
+        orientation: 'portrait',
+        lang: 'pl'
+      },
+      devOptions: {
+        enabled: true
+      }
+    }),
+    VueDevTools()
   ],
   resolve: {
     alias: {
