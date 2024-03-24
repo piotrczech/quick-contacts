09fe0c1cb95a467e096d16fd382f60cfb7295f61
Piotr Czech <czechpiotr2701@gmail.com>
2024-03-24 15:40:33 +0100

    feat: add routing for add new contact info view

---
+++ src/views/contact-info/ContactInfoAddView.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+  <v-container fluid>
+    <v-row>
+      <v-col cols="12" class="d-flex justify-center"> add </v-col>
+    </v-row>
+  </v-container>
+</template>
