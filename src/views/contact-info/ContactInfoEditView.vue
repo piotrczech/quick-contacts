965b2b2b39df9519eeec708b1d04e13158738c87
Piotr Czech <czechpiotr2701@gmail.com>
2024-03-24 16:37:52 +0100

    feat: add routing and view for editing contact info

---
+++ src/views/contact-info/ContactInfoEditView.vue
@@ -0,0 +1,19 @@
+<script setup>
+import ContactInfoDataForm from '@/components/contact-info/ContactInfoDataForm.vue'
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+const contactInfoId = computed(() => route.params.id)
+</script>
+
+<template>
+  <v-container fluid>
+    <v-row>
+      <v-col cols="12" class="d-flex justify-center">
+        {{ contactInfoId }}
+        <contact-info-data-form />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
